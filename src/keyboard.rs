7a444c872ffb758062444e509fa28ce8f1fff16d
Matthew Bland <mabla0531@gmail.com>
2025-11-08 22:21:19 -0600

    add autocomplete for url bar

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -11,6 +11,20 @@ impl Application {
     pub fn handle_input(&mut self, event: KeyEvent) {
         match event.code {
             KeyCode::Char(character) => match self.state.focused_panel {
+                Panel::Method => {
+                    self.state.current_method = match character {
+                        'g' | 'G' => Method::Get,
+                        'p' | 'P' => Method::Post,
+                        'o' | 'O' => Method::Options,
+                        'c' | 'C' => Method::Connect,
+                        't' | 'T' => Method::Trace,
+                        'd' | 'D' => Method::Delete,
+                        'h' | 'H' => Method::Head,
+                        'u' | 'U' => Method::Put,
+                        'a' | 'A' => Method::Patch,
+                        _ => self.state.current_method.clone(),
+                    }
+                }
                 Panel::Url => self.state.url_input.push(character),
                 _ => {}
             },
@@ -26,6 +40,7 @@ impl Application {
                 self.state.url_input.pop();
             }
             KeyCode::Tab => {
+                self.state.show_method_dropdown = false;
                 self.state.focused_panel = match self.state.focused_panel {
                     Panel::Method => Panel::Url,
                     Panel::Url => Panel::Request,
@@ -89,6 +104,19 @@ impl Application {
                 _ => {}
             },
             KeyCode::Right => match self.state.focused_panel {
+                Panel::Url => {
+                    if self.state.url_input == "https" || self.state.url_input == "http" {
+                        self.state.url_input.push_str("://");
+                    }
+                    if "htt".starts_with(self.state.url_input.as_str())
+                        || self.state.url_input.is_empty()
+                    {
+                        self.state.url_input = "http".to_string();
+                    }
+                    if self.state.url_input.ends_with(".") {
+                        self.state.url_input.push_str("com");
+                    }
+                }
                 Panel::Request => {
                     self.state.current_request_tab = match self.state.current_request_tab {
                         RequestTab::Headers => RequestTab::Body,
--- src/ui/components/url_input.rs
+++ src/ui/components/url_input.rs
@@ -16,10 +16,24 @@ impl Application {
             Default::default()
         };
 
-        let url = Paragraph::new(Line::from_iter([Span::styled(
-            self.state.url_input.clone(),
-            Style::default().bg(palette::CRUST),
-        )]))
+        let help_string = if self.state.focused_panel != Panel::Url {
+            String::new()
+        } else if self.state.url_input == "https" || self.state.url_input == "http" {
+            ":// 󰛂".to_string()
+        } else if "htt".starts_with(self.state.url_input.as_str())
+            || self.state.url_input.is_empty()
+        {
+            "http 󰛂".replace(self.state.url_input.as_str(), "")
+        } else if self.state.url_input.ends_with(".") {
+            "com 󰛂".to_string()
+        } else {
+            String::new()
+        };
+
+        let url = Paragraph::new(Line::from_iter([
+            Span::styled(self.state.url_input.clone(), Style::default()),
+            Span::styled(help_string, Style::default().fg(palette::SUBTEXT0)),
+        ]))
         .block(
             Block::new()
                 .borders(Borders::ALL)
